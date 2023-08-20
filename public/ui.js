10f35e0e7c8c69511f9ba48eea3a8699b467b774
shoebilyas123 <shoebilyas123@gmail.com>
2023-08-20 09:28:30 +0530

    feat(*): DOM init

---
+++ public/ui.js
@@ -0,0 +1,29 @@
+// Set height and width of the canvas equal to window;
+
+let wHeight = window.innerHeight;
+let wWidth = window.innerWidth;
+console.log({ wHeight, wWidth });
+
+const canvas = document.getElementById('the-canvas');
+const context = canvas.getContext('2d');
+
+const player = {}; // "this" player
+
+canvas.width = wWidth;
+canvas.height = wHeight;
+
+const loginModal = new bootstrap.Modal(document.querySelector('#loginModal'));
+const spawnModal = new bootstrap.Modal(document.querySelector('#spawnModal'));
+
+window.addEventListener('load', () => {
+  loginModal.show();
+});
+
+document.querySelector('.name-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  player.name = document.querySelector('#name-input').value;
+  console.log({ player });
+  loginModal.hide();
+  document.querySelector('.player-name').innerText = player.name;
+  spawnModal.show();
+});
