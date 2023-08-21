9990354b36fcf9919914e3aee5864a9303174e5a
shoebilyas123 <shoebilyas123@gmail.com>
2023-08-21 11:39:59 +0530

    feat(game): draw the player

--- public/canvas.js
+++ public/canvas.js
@@ -1,1 +1,14 @@
-const init = () => {};
+const init = () => {
+  draw();
+};
+
+let randomX = Math.floor(500 * Math.random() + 10);
+let randomY = Math.floor(500 * Math.random() + 10);
+context.fillStyle = 'rgb(255,0,0)';
+context.arc(randomX, randomY, 10, 0, 2 * Math.PI);
+console.log({ randomX });
+context.fill();
+context.lineWidth = 3;
+context.strokeStyle = 'rgb(0,255,0)';
+context.stroke();
+const draw = () => {};
